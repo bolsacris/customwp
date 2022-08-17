a549c5badce537249150ae8de9d974f6f28dfe39
bolsacris <105169927+bolsacris@users.noreply.github.com>
2022-08-18 00:13:59 +0200

    Add files via upload

--- customwp.js
+++ customwp.js
@@ -1,29 +1,20 @@
-Hooks.on('ready', () => {
-	CONFIG.DND5E.weaponProperties['antiArmor'] = 'Anti-Armor';
-	CONFIG.DND5E.weaponProperties['concealable'] = 'Concealable';
-	CONFIG.DND5E.weaponProperties['disarming'] = 'Disarming';
-	CONFIG.DND5E.weaponProperties['holdout'] = 'Holdout';
-	CONFIG.DND5E.weaponProperties['impactful'] = 'Impactful';
-	CONFIG.DND5E.weaponProperties['jointLocking'] = 'Joint-locking';
-	CONFIG.DND5E.weaponProperties['lobbed'] = 'Lobbed';
-	CONFIG.DND5E.weaponProperties['momentum'] = 'Momentum';
-	CONFIG.DND5E.weaponProperties['parrying'] = 'Parrying';
-	CONFIG.DND5E.weaponProperties['shoving'] = 'Shoving';
-	CONFIG.DND5E.weaponProperties['sweeping'] = 'Sweeping';
-	CONFIG.DND5E.weaponProperties['tripping'] = 'Tripping';
-	CONFIG.DND5E.weaponProperties['rangedTripping'] = 'Ranged_Tripping';
-	CONFIG.DND5E.weaponProperties['unhorsing'] = 'Unhorsing';
-	CONFIG.DND5E.weaponProperties['dragonHorn'] = 'Dragon-horn';
-	CONFIG.DND5E.weaponProperties['iron'] = 'Iron';
-	CONFIG.DND5E.weaponProperties['mithral'] = 'Mithral';
-	CONFIG.DND5E.weaponProperties['tether'] = 'Tether';
+Hooks.on("init", () => {
+	CONFIG.DND5E.weaponProperties["antiArmor"] = game.i18n.localize("CWP.antiArmor");
+	CONFIG.DND5E.weaponProperties["concealable"] = game.i18n.localize("CWP.concealable");
+	CONFIG.DND5E.weaponProperties["disarming"] = game.i18n.localize("CWP.disarming");
+	CONFIG.DND5E.weaponProperties["holdout"] = game.i18n.localize("CWP.holdout");
+	CONFIG.DND5E.weaponProperties["impactful"] = game.i18n.localize("CWP.impactful");
+	CONFIG.DND5E.weaponProperties["jointLocking"] = game.i18n.localize("CWP.jointLocking");
+	CONFIG.DND5E.weaponProperties["lobbed"] = game.i18n.localize("CWP.lobbed");
+	CONFIG.DND5E.weaponProperties["momentum"] = game.i18n.localize("CWP.momentum");
+	CONFIG.DND5E.weaponProperties["parrying"] = game.i18n.localize("CWP.parrying");
+	CONFIG.DND5E.weaponProperties["shoving"] = game.i18n.localize("CWP.shoving");
+	CONFIG.DND5E.weaponProperties["sweeping"] = game.i18n.localize("CWP.sweeping");
+	CONFIG.DND5E.weaponProperties["tripping"] = game.i18n.localize("CWP.tripping");
+	CONFIG.DND5E.weaponProperties["rangedTripping"] = game.i18n.localize("CWP.rangedTripping");
+	CONFIG.DND5E.weaponProperties["unhorsing"] = game.i18n.localize("CWP.unhorsing");
+	CONFIG.DND5E.weaponProperties["dragonHorn"] = game.i18n.localize("CWP.dragonHorn");
+	CONFIG.DND5E.weaponProperties["iron"] = game.i18n.localize("CWP.iron");
+	CONFIG.DND5E.weaponProperties["mithral"] = game.i18n.localize("CWP.mithral");
+	CONFIG.DND5E.weaponProperties["tether"] = game.i18n.localize("CWP.tether");
 });
-
-
-
-
-
-
-
-
-
