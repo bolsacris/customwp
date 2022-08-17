be979ed21b39082ea29244dacb4c5ac482b20c4b
bolsacris <105169927+bolsacris@users.noreply.github.com>
2022-08-17 17:13:38 +0200

    Add files via upload

---
+++ customwp.js
@@ -0,0 +1,29 @@
+Hooks.on('ready', () => {
+	CONFIG.DND5E.weaponProperties['antiArmor'] = 'Anti-Armor';
+	CONFIG.DND5E.weaponProperties['concealable'] = 'Concealable';
+	CONFIG.DND5E.weaponProperties['disarming'] = 'Disarming';
+	CONFIG.DND5E.weaponProperties['holdout'] = 'Holdout';
+	CONFIG.DND5E.weaponProperties['impactful'] = 'Impactful';
+	CONFIG.DND5E.weaponProperties['jointLocking'] = 'Joint-locking';
+	CONFIG.DND5E.weaponProperties['lobbed'] = 'Lobbed';
+	CONFIG.DND5E.weaponProperties['momentum'] = 'Momentum';
+	CONFIG.DND5E.weaponProperties['parrying'] = 'Parrying';
+	CONFIG.DND5E.weaponProperties['shoving'] = 'Shoving';
+	CONFIG.DND5E.weaponProperties['sweeping'] = 'Sweeping';
+	CONFIG.DND5E.weaponProperties['tripping'] = 'Tripping';
+	CONFIG.DND5E.weaponProperties['rangedTripping'] = 'Ranged Tripping';
+	CONFIG.DND5E.weaponProperties['unhorsing'] = 'Unhorsing';
+	CONFIG.DND5E.weaponProperties['dragonHorn'] = 'Dragon-horn';
+	CONFIG.DND5E.weaponProperties['iron'] = 'Iron';
+	CONFIG.DND5E.weaponProperties['mithral'] = 'Mithral';
+	CONFIG.DND5E.weaponProperties['tether'] = 'Tether';
+});
+
+
+
+
+
+
+
+
+
